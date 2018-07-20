3f26a97f1b2960a295f0d5ac22c14b72cfae4bbf
mapsedge <41483625+mapsedge@users.noreply.github.com>
2018-07-20 11:27:02 -0500

    Update deleteInPlace.1.0.js

--- deleteInPlace.1.0.js
+++ deleteInPlace.1.0.js
@@ -1,11 +1,11 @@
-// 2018, Jul 09 - BRM
-
+// Copyright 2018, William R. Morris.
+;
 (function($) {
 
     // here we go!
     $.deleteInPlace = function(element, options) {
 
-		var attributesToIgnore = ['class', 'id'];
+	var attributesToIgnore = ['class', 'id'];
 	
         var defaults = {
 
@@ -19,21 +19,13 @@
 
         var plugin = this;
 
-        // this will hold the merged default, and user-provided options
-        // plugin's properties will be available through this object like:
-        // plugin.settings.propertyName from inside the plugin or
-        // element.data('deleteInPlace').settings.propertyName from outside the plugin, 
-        // where "element" is the element the plugin is attached to;
         plugin.settings = {}
 
         var $element = $(element), // reference to the jQuery version of DOM element
              element = element;    // reference to the actual DOM element
 		
-        // the "constructor" method that gets called when the object is created
         plugin.init = function() {
 
-            // the plugin's final properties are the merged default and 
-            // user-provided options (if any)
             plugin.settings = $.extend({}, defaults, options);
 
 			var buttonOk = $("<a class='dp-confirm'>" + plugin.settings.label.ok + "</a>");
@@ -41,8 +33,7 @@
 			var container1 = $("<div class='controlset controlset1'></div>");
 			var container2 = $("<div class='controlset controlset2'></div>");
 			var cssStyles = null;
-			
-            // code goes here
+
 			var C = $element;
 			//
 			var a = $("<div class='dp-controls'></div>");
@@ -101,61 +92,31 @@
 
         }
 
-        // public methods
-        // these methods can be called like:
-        // plugin.methodName(arg1, arg2, ... argn) from inside the plugin or
-        // element.data('deleteInPlace').publicMethod(arg1, arg2, ... argn) from outside 
-        // the plugin, where "element" is the element the plugin is attached to;
-
-        // a public method. for demonstration purposes only - remove it!
-        plugin.foo_public_method = function() {
-
-            // code goes here
-
-        }
-
-        // private methods
-        // these methods can be called only from inside the plugin like:
-        // methodName(arg1, arg2, ... argn)
-
-        // a private method. for demonstration purposes only - remove it!
-        var private_foo = function() {
-            // code goes here
-
-        }
-
-        // fire up the plugin!
-        // call the "constructor" method
         plugin.init();
 
     }
 
-    // add the plugin to the jQuery.fn object
     $.fn.deleteInPlace = function(options) {
 
-		var sheet = document.styleSheets[0];
-		sheet.insertRule('.dp-controls { transition: opacity .5s; opacity: .2; cursor: default; display: inline-block; position: relative; padding: 1em; }', 0);
-		sheet.insertRule('.dp-controls>div { position: absolute; left: 0; top: 0; font-size: 1.2em;}', 0);
-		sheet.insertRule('.dp-controls * { font-size: 1em;}', 0);
-		sheet.insertRule('.dp-controls>div.controlset2 { display:none;white-space:nowrap; }', 0);
-		sheet.insertRule('.dp-controls:hover { opacity: 1; }', 0);
-		sheet.insertRule('.dp-confirm {font-weight: bold; color:#080;}', 0);
-		sheet.insertRule('.dp-confirm:hover {color:#0F0;}', 0);
-		sheet.insertRule('.dp-cancel {color:#A00;}', 0);
-		sheet.insertRule('.dp-cancel:hover {color:#F00;}', 0);
-		sheet.insertRule('.dp-confirmmsg {margin-right: 1em;}', 0);
+	var sheet = document.styleSheets[0];
+	sheet.insertRule('.dp-controls { transition: opacity .5s; opacity: .2; cursor: default; display: inline-block; position: relative; padding: 1em; }', 0);
+	sheet.insertRule('.dp-controls>div { position: absolute; left: 0; top: 0; font-size: 1.2em;}', 0);
+	sheet.insertRule('.dp-controls * { font-size: 1em;}', 0);
+	sheet.insertRule('.dp-controls>div.controlset2 { display:none;white-space:nowrap; }', 0);
+	sheet.insertRule('.dp-controls:hover { opacity: 1; }', 0);
+	sheet.insertRule('.dp-confirm {font-weight: bold; color:#080;}', 0);
+	sheet.insertRule('.dp-confirm:hover {color:#0F0;}', 0);
+	sheet.insertRule('.dp-cancel {color:#A00;}', 0);
+	sheet.insertRule('.dp-cancel:hover {color:#F00;}', 0);
+	sheet.insertRule('.dp-confirmmsg {margin-right: 1em;}', 0);
 
-        // iterate through the DOM elements we are attaching the plugin to
         return this.each(function() {
-			// 'this' is the list of elements being affected
-
             // if plugin has not already been attached to the element
             if (undefined == $(this).data('deleteInPlace')) {
-				// $(this) refers to the element we're working on RIGHT NOW as a JQUERY object
-				
+		// $(this) refers to the element we're working on RIGHT NOW as a JQUERY object
                 // create a new instance of the plugin
                 // pass the DOM element and the user-provided options as arguments
-				// "this", in this case, is a non-JQUERY DOM element
+		// "this", in this case, is a non-JQUERY DOM element
                 var plugin = new $.deleteInPlace(this, options);
 
                 // in the jQuery version of the element
